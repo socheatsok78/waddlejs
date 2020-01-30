64441a5422e0b5eee3adf886fb7b0daa967e2668
Socheat Sok <alex@socheat.net>
2020-01-30 15:39:52 +0700

    Create /lib/utils/Listener.js, and add export to index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+// Main
 export { default as ServiceWorkerManager } from './lib/ServieWorkerManager';
 export { default as MessageBus } from "./lib/MessageBus";
+
+// Utils
+export { default as Listener } from './lib/utils/Listener';
